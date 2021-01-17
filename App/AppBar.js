52956dfb19adcf66f4cb80b5ce653992ca82942f
TLeD8 <tyler.a.leduc@gmail.com>
2021-01-17 12:00:47 -0700

    Fixed change price; added AppBar.js

---
+++ App/AppBar.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import { AppContext } from './AppProvider';
+
+const Logo = styled.div`
+  font-size: 1.5em;
+`;
+
+// auto will replace remaining content with space
+const Bar = styled.div`
+  display: grid;
+  margin-bottom: 40px;
+  grid-template-columns: 180px auto 100px 100px;
+`;
+
+const ControlButtonElem = styled.div`
+  cursor: pointer;
+  ${props =>
+    props.active &&
+    css`
+      text-shadow: 0px 0px 60px #03ff03;
+    `}
+  ${props =>
+    props.hidden &&
+    css`
+      display: none;
+    `}
+`;
+
+
+function ChangePercent({ data }) {
+  return (
+    <JustifyRight>
+      <ChangePct red={data.CHANGEPCT24HOUR < 0}>
+        {numberFormat(data.CHANGEPCT24HOUR)} %
+      </ChangePct>
+    </JustifyRight>
+  );
+}
+
+// Capitalises first letter of word
+function toProperCase(lower) {
+  return lower.charAt(0).toUpperCase() + lower.substr(1);
+}
+
+function ControlButton({ name, active }) {
+  return (
+    <AppContext.Consumer>
+      {({ firstVisit, page, setPage }) => (
+        <ControlButtonElem
+          active={page === name}
+          onClick={() => setPage(name)}
+          hidden={firstVisit && name === 'dashboard'}
+        >
+          {toProperCase(name)}
+        </ControlButtonElem>
+      )}
+    </AppContext.Consumer>
+  );
+}
+
+export default function() {
+  return (
+    <Bar>
+      <Logo> CryptoDashboard </Logo>
+      <div />
+      <ControlButton active name="dashboard" />
+      <ControlButton name="settings" />
+    </Bar>
+  );
+}
